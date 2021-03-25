9ead058d579d09c2f4e52f4d981d2653d89fb4f3
Anton <antoniovolkoff@gmail.com>
2021-03-25 21:41:03 +0300

    Initial Commit

---
+++ Calculyator/CalculyatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculyatorApp.swift
+//  Calculyator
+//
+//  Created by Антон on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculyatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
